b099e098be35edef8d1c5dd1b23955f773be4570
wuxl <wuxl@archempower.com>
2022-06-27 16:08:04 +0800

    update

--- electronFakeData/main.js
+++ electronFakeData/main.js
@@ -1,34 +1,50 @@
 const { app, BrowserWindow, ipcMain } = require("electron");
 
 let mainWindow = null;
-const exec = require("child_process").exec;
 
-// 任何你期望执行的cmd命令，ls都可以
-let cmdStr = "./node_modules/.bin/nodemon ./config/run";
-// 执行cmd命令的目录，如果使用cd xx && 上面的命令，这种将会无法正常退出子进程
-let cmdPath = "../service";
+// 关闭后台服务
+function stopServer() {
+  const kill = require("tree-kill");
+  if (serverProcess) {
+    logger.info(
+      "kill server process , serverProcess.pid-->",
+      serverProcess.pid
+    );
+
+    kill(serverProcess.pid, "SIGTERM", function () {
+      serverProcess = null;
+      logger.info("后台服务已关闭...");
+    });
+  }
+}
 // 子进程名称
-let workerProcess;
+let serverProcess = null;
+function startServer() {
+  // 启动后台服务的命令
+  let cmdStr = "./node_modules/.bin/nodemon ./config/run";
+  // 启动路径
+  let cmdPath = "../service";
+  // 区分测试环境与生产环境
+  //   let cmdPath = isDevelopment? "./server":"./resuorces/server"
+  runExec(cmdStr);
 
-function runExec() {
-  // 执行命令行，如果命令不需要路径，或就是项目根目录，则不需要cwd参数：
-  workerProcess = exec(cmdStr, { cwd: cmdPath });
-  // 不受child_process默认的缓冲区大小的使用方法，没参数也要写上{}：workerProcess = exec(cmdStr, {})
-
-  // 打印正常的后台可执行程序输出
-  workerProcess.stdout.on("data", function (data) {
-    console.log("stdout: " + data);
-  });
-
-  // 打印错误的后台可执行程序输出
-  workerProcess.stderr.on("data", function (data) {
-    console.log("stderr: " + data);
-  });
-
-  // 退出之后的输出
-  workerProcess.on("close", function (code) {
-    console.log("out code：" + code);
-  });
+  function runExec(cmdStr) {
+    // 在启动后台服务前闲检测关闭一遍后台服务，防止开启多个后台服务
+    stopServer();
+    serverProcess = require("child_process").spawn(cmdStr, { cwd: cmdPath });
+    // 启动成功的输出
+    serverProcess.stdout.on("data", function (data) {
+      console.log("启动服务器成功！ stdout:" + data);
+    });
+    // 发生错误的输出
+    serverProcess.stderr.on("data", function (data) {
+      console.log("stderr:" + data);
+    });
+    // 退出后的输出
+    serverProcess.on("close", function (code) {
+      console.log("out code:" + code);
+    });
+  }
 }
 
 function createWindow() {
@@ -54,13 +70,13 @@ function createWindow() {
 
   mainWindow.loadURL("http://127.0.0.1:3001/"); //根据自己的端口和ip修改 //打开开发者模式，开发环境请打开方便调试，生产环境需要禁用。
 
-  // mainWindow.webContents.openDevTools(); //判断是否是开发模式
+  mainWindow.webContents.openDevTools(); //判断是否是开发模式
 }
 
 //app主进程的事件和方法
 
 app.on("ready", () => {
-  runExec() // 生效啦，可以做些什么执行一种相对的同步状态，例如判断输出内容到什么了
+  startServer();
   createWindow();
 });
 
